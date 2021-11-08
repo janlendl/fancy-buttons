e6eba72fb1ed5b4f158ddbc28370a8f39b6f3b6d
Lendl Peralta <janlendl@gmail.com>
2021-11-07 20:24:46 -0700

    Add event handler to increment by 1 for every click

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
 function CounterButton() {
-  const [clickAmount, setClickAmount] = useState();
+  const [clickAmount, setClickAmount] = useState(0);
+  const handleClick = () => {
+    setClickAmount(clickAmount + 1);
+  };
+
   return (
-    <button className="CounterButton">
-      You clicked me X amount of times
+    <button onClick={ handleClick } className="CounterButton">
+      You clicked me { clickAmount } amount of times
     </button>    
   );
 };
