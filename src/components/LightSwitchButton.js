b1091a3f1a2e1d9d0e17866e40db79a915ebf073
Lendl Peralta <janlendl@gmail.com>
2021-11-07 20:54:48 -0700

    Use props to call the use state

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,15 +1,15 @@
 import { useState } from "react";
 
-function LightSwitchButton() {
-  const [light, setLight] = useState('off');
+function LightSwitchButton(props) {
+
   const handleClick = () => {
-    setLight(light === 'on' ? 'off' : 'on');
+    props.setLight(props.light === 'on' ? 'off' : 'on');
   };
 
   return (
     <button onClick={ handleClick } className="LightSwitchButton">
-      {light === "on" && <span><i>💡</i> I'm on!</span>}
-      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+      {props.light === "on" && <span><i>💡</i> I'm on!</span>}
+      {props.light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
   );
 };
